5fb1a66dfdaf7c70eecde11f087f5bd7aab30d3d
Mani85861 <usmani85861@gmail.com>
2026-04-09 23:45:41 +0500

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -39,7 +39,7 @@ cmd({
             forwardingScore: 999,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363318478753709@newsletter',
+                newsletterJid: '120363408207081912@newsletter',
                 newsletterName: config.OWNER_NAME,
                 serverMessageId: 143
             }
@@ -51,7 +51,7 @@ cmd({
                 return await conn.sendMessage(
                     from,
                     {
-                        image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/p4yxcn.png' },
+                        image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/y67z0s.jpg' },
                         caption: menuCaption,
                         contextInfo: contextInfo
                     },
@@ -418,7 +418,7 @@ cmd({
                                 await conn.sendMessage(
                                     senderID,
                                     {
-                                        image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/p4yxcn.png' },
+                                        image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/y67z0s.jpg' },
                                         caption: selectedMenu.content,
                                         contextInfo: contextInfo
                                     },
