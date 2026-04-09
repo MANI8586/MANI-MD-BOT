c66f4383b69bb4c267d3a274d2df0d4f064b3bfc
Mani85861 <usmani85861@gmail.com>
2026-04-10 00:07:05 +0500

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -3,7 +3,7 @@ const { sleep } = require("../lib/functions");
 
 cmd({  
     pattern: "restart",  
-    desc: "Restart Arslan_MD",  
+    desc: "Restart MANI X MD",  
     category: "owner",  
     filename: __filename  
 },  
